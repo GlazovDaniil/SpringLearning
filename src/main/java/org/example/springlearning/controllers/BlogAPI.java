a19c20c951a66eae07b1715410cd9fef4a187847
GlazovDaniil <d.glazow2016@yandex.ru>
2024-03-06 02:26:45 +0300

    0.3

--- src/main/java/org/example/springlearning/controllers/BlogAPI.java
+++ src/main/java/org/example/springlearning/controllers/BlogAPI.java
@@ -1,69 +1,62 @@
 package org.example.springlearning.controllers;
 
+import io.micrometer.common.KeyValue;
 import org.example.springlearning.models.Post;
 import org.example.springlearning.repo.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+@RestController
+public class BlogAPI {
 
-@Controller
-public class BlogController {
+    @GetMapping(path = "api")
+    public List<String> list(){
+        return List.of("Hello", "world") ;
+    }
 
     @Autowired
     private PostRepository postRepository;
 
-    @GetMapping("/blog")
-    public String blogMain(Model model){
+    @GetMapping("/api/blog")
+    public Iterable<Post> blogMain(Model model){
         Iterable<Post> posts = postRepository.findAll();
-        model.addAttribute("posts", posts);
-        return "blog-main";
+        return posts;
     }
 
-    @GetMapping("/blog/add")
-    public String blogAdd(Model model){
-        return "blog-add";
-    }
 
-    @PostMapping("/blog/add")
+    @PostMapping("/api/blog/add")
     public String blogPostAdd(@RequestParam String title,
                               @RequestParam String anons,
                               @RequestParam String full_text,
                               Model model) {
         Post post = new Post(title, anons, full_text);
         postRepository.save(post);
-        return "redirect:/blog";
+
+        return "{ \n\"title\": " + title + ",\n\"anons\": " + anons + ",\n\"full_text\": " + full_text + "\n}";
     }
 
-    @GetMapping("/blog/{id}")
-    public String blogDetail(Model model, @PathVariable(value = "id") long id){
+    @GetMapping("/api/blog/{id}")
+    public ArrayList<Post> blogDetail(Model model, @PathVariable(value = "id") long id){
+        ArrayList<Post> res = new ArrayList<>();
         if(!postRepository.existsById(id)) {
-            return "redirect:/blog";
+            return res;//"{\"Error\": В базе данных нет поста с таким ID. }";
         }
         Optional<Post> post = postRepository.findById(id);
-        ArrayList<Post> res = new ArrayList<>();
         post.ifPresent(res::add);
-        model.addAttribute("post", res);
-        return "blog-details";
+        return res;
     }
 
-    @GetMapping("/blog/{id}/edit")
-    public String blogEdit(Model model, @PathVariable(value = "id") long id){
-        if(!postRepository.existsById(id)) {
-            return "redirect:/blog";
-        }
-        Optional<Post> post = postRepository.findById(id);
-        ArrayList<Post> res = new ArrayList<>();
-        post.ifPresent(res::add);
-        model.addAttribute("post", res);
-        return "blog-edit";
+    @GetMapping("/api/blog/{id}/edit")
+    public ArrayList<Post> blogEdit(Model model, @PathVariable(value = "id") long id){
+        return blogDetail(model, id);
     }
 
-    @PostMapping("/blog/{id}/edit")
+    @PutMapping("/api/blog/{id}/edit")
     public String blogPostUpdate(Model model,
                                  @PathVariable(value = "id") long id,
                                  @RequestParam String title,
@@ -74,13 +67,14 @@ public class BlogController {
         post.setAnons(anons);
         post.setFull_text(full_text);
         postRepository.save(post);
-        return "redirect:/blog";
+        return "{ \n\"title\": " + title + ",\n\"anons\": " + anons + ",\n\"full_text\": " + full_text + "\n}";
     }
 
-    @PostMapping("/blog/{id}/remove")
+    @DeleteMapping("/api/blog/{id}/remove")
     public String blogDelete(Model model, @PathVariable(value = "id") long id){
         Post post = postRepository.findById(id).orElseThrow();
         postRepository.delete(post);
-        return "redirect:/blog";
+        return "{ \"message\": Запись удалена }";
     }
+
 }
